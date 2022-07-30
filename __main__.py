530452b6d7bba5dcdacb790c7c9c7cf8255201c1
retkiewi <58555777+retkiewi@users.noreply.github.com>
2022-07-30 22:44:43 +0200

    add flush

--- __main__.py
+++ __main__.py
@@ -10,7 +10,7 @@ def main():
         service.start()
 
         while service.is_alive():
-            print(client_socket.listen(1))
+            print(client_socket.listen(1), flush=True)
             
             time.sleep(5)
 
